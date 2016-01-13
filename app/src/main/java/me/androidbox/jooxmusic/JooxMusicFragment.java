9ca43fa4ece7039d3425d9bb52d99f163db86cec
Steve <steve1.rm@gmail.com>
2016-01-13 23:54:24 +0700

    Added toolbar and tablayout

--- app/src/main/java/me/androidbox/jooxmusic/JooxMusicFragment.java
+++ app/src/main/java/me/androidbox/jooxmusic/JooxMusicFragment.java
@@ -2,17 +2,18 @@ package me.androidbox.jooxmusic;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class JooxMusicFragment extends Fragment {
-
 
     public JooxMusicFragment() {
         // Required empty public constructor
@@ -21,7 +22,22 @@ public class JooxMusicFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_joox_music, container, false);
+        final View view = inflater.inflate(R.layout.fragment_joox_music, container, false);
+
+        /* Add the toolbar */
+        final AppCompatActivity appCompatActivity = (AppCompatActivity)getActivity();
+        final Toolbar toolbar = (Toolbar)view.findViewById(R.id.tbJooxMusic);
+        appCompatActivity.setSupportActionBar(toolbar);
+
+        /* Remove the title from the toolbar */
+        appCompatActivity.getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        /* Add the tabs */
+        TabLayout tabLayout = (TabLayout)view.findViewById(R.id.tlJooxMusic);
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_music_video_white_36dp));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_youtube_searched_for_white_36dp));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_radio_white_36dp));
+        return view;
     }
 
 }
